632f386aa6e3cd2351842042136bc2180452cffe
Bung Danar <danar.g.putera@gmail.com>
2023-06-02 11:44:49 +0700

    chore: Add vehicle update endpoint

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -15,11 +15,37 @@ namespace aspnetcore_tutorial.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreateVehicle(VehicleResource vehicleResource)
+        public async Task<ActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid modelId.");
+                return BadRequest(ModelState);
+            }
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdated = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.VehicleFeatures).SingleOrDefaultAsync(v => v.Id == id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdated = DateTime.Now;
+
             await context.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -19,15 +19,42 @@ namespace aspnetcore_tutorial.Mapping
 
             // API Resource to Domain
             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
                 .ForMember(v => v.Contact, opt => opt.MapFrom(vr => new Contact
                 {
                     Name = vr.Contact.Name,
                     Email = vr.Contact.Email,
                     Phone = vr.Contact.Phone
                 }))
-                // .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new Feature { Id = id })));
                 .ForMember(v => v.Features, opt => opt.Ignore())
-                .ForMember(v => v.VehicleFeatures, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+                // .ForMember(v => v.VehicleFeatures, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })))
+                .ForMember(v => v.VehicleFeatures, opt => opt.Ignore())
+                .AfterMap((vr, v) =>
+                {
+                    // Remove unselected features
+                    var removedFeatures = new List<VehicleFeature>();
+                    foreach (var f in v.VehicleFeatures)
+                    {
+                        if (!vr.Features.Contains(f.FeatureId))
+                        {
+                            removedFeatures.Add(f);
+                        }
+                    }
+
+                    foreach (var f in removedFeatures)
+                    {
+                        v.VehicleFeatures.Remove(f);
+                    }
+
+                    // Add new features
+                    foreach (var id in vr.Features)
+                    {
+                        if (!v.VehicleFeatures.Any(f => f.FeatureId == id))
+                        {
+                            v.VehicleFeatures.Add(new VehicleFeature { FeatureId = id });
+                        }
+                    }
+                });
 
         }
     }
